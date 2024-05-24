ff6e104526e24a76781022b28255513403b7097b
ac-gomes <acgoms88@gmail.com>
2024-05-24 17:24:34 -0300

    summarize documents t5-small model

--- hugging_face/summarizer.py
+++ hugging_face/summarizer.py
@@ -14,23 +14,23 @@ xsum_dataset = load_dataset(
     trust_remote_code=True
 )
 
-xsum_sample = xsum_dataset["train"].select(range(10))
-
-# display(
-#     xsum_sample.to_pandas()
-# )
-
 summarizer = pipeline(
     task="summarization",
     model=model,
     min_length=20,
     max_length=40,
     truncation=True,
-    # model_kwargs={cache_di},
+
 )
 
-output = summarizer(
-    xsum_sample["document"][0]
-)
+xsum_sample = xsum_dataset["train"].select(range(5))
 
-print(output)
+output = summarizer(xsum_sample["document"])
+
+display(
+    pd.DataFrame.from_dict(output)
+    .rename({"summary_text": "generated_summary"}, axis=1)
+    .join(pd.DataFrame.from_dict(xsum_sample))[
+        ["generated_summary", "summary", "document"]
+    ]
+)
